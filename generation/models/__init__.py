fa1b68bbb54a21965628972b0ed46ca0266aa6b6
EmanuelLM <73854070+EmanuelLM@users.noreply.github.com>
2021-01-30 12:57:35 -0500

    EfficientNet classifier

--- generation/models/__init__.py
+++ generation/models/__init__.py
@@ -8,6 +8,18 @@ from tqdm import tqdm
 import numpy as np
 from . import vae, deepinfomax
 import time
+
+__version__ = "0.7.0"
+from .model import EfficientNet, VALID_MODELS
+from .utils import (
+    GlobalParams,
+    BlockArgs,
+    BlockDecoder,
+    efficientnet,
+    get_model_params,
+)
+
+
 try:
     from apex import amp
 except:
@@ -20,3 +32,4 @@ def get_model(exp_dict, **kwargs):
         return deepinfomax.DeepInfoMax(exp_dict, **kwargs)
     else:
         raise ValueError("Model %s not found" %exp_dict["model"])
+        
