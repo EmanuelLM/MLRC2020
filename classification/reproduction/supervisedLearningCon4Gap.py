32ae3a0844f09a8b9c5356001c1bed1580b02176
Jingyi Xiao <xiaojy_ann@126.com>
2021-10-06 13:40:41 -0400

    add plot loss function in Con4Gap model

---
+++ classification/reproduction/supervisedLearningCon4Gap.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import pickle as pkl
+import matplotlib.pyplot as plt
+
+
+! git clone https://github.com/ElementAI/synbols-benchmarks.git
+
+# enter into classification (supervised learning) experiment
+os.chdir('/content/drive/MyDrive/synbols-benchmarks/classification')
+!pip install -r requirements.txt
+
+# reproduce the results with seed 3
+! python3 trainval.py --savedir_base '/content/drive/MyDrive/CONV-4-flat' --exp_group_list baselines
+
+
+### plot for train loss and val loss
+
+#---- conv-4-GAP ----#
+#change the file path to your customized path
+files = [
+         '/content/drive/MyDrive/conv_4_GAP/a5728d39488d9b5572267b8825652a69', #default100k
+         '/content/drive/MyDrive/conv_4_GAP/1494ba72bf70b3e324ed26090c4f71a8', #camouflage
+         '/content/drive/MyDrive/conv_4_GAP/bcd2870382d348402a0ff290f04fa824', #korean
+         '/content/drive/MyDrive/conv_4_GAP/d772582b13d58e435fd2f54f7bc0ec82' #less_variation
+         ]
+
+task_genre =['default100k',
+             'camouflage',
+             'korean',
+             'lessvar'
+             ]
+colors = [
+          'orange',
+          'xkcd:cyan',
+          'xkcd:red',
+          'xkcd:blue'
+          ]
+
+for (i, task) in zip(files, task_genre):
+  add_path = f'{i}/score_list.pkl'
+  globals()['train_loss_' + 'conv4GAP_' + task] = []
+  globals()['valid_loss_' + 'conv4GAP_' + task] = []
+  with open(add_path, 'rb') as temp_file:
+    temp_data = pkl.load(temp_file)
+    for j in temp_data:
+        globals()['train_loss_' + 'conv4GAP_' + str(task)].append(j['train_loss'])
+        globals()['valid_loss_' + 'conv4GAP_' + str(task)].append(j['val_loss'])
